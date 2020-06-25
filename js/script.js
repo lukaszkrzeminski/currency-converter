1d78ae831c1c74d927c69837d93ed38c9d49e1f0
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-06-25 21:51:40 +0200

    script.js divided into functions

--- js/script.js
+++ js/script.js
@@ -1,52 +1,53 @@
-let userValueElement = document.querySelector(".js-userValue");
-let userCurrencyElement = document.querySelector(".js-userCurrency");
-let chosenCurrencyElement = document.querySelector(".js-chosenCurrency");
-let finalValueElement = document.querySelector(".js-finalValue");
-let formElement = document.querySelector(".js-form");
+{
+    const computeResult = (startCurrency, finalCurrency, startValue) => {
+        const PLNtoEUR = 0.22;
+        const PLNtoUSD = 0.25;
+        const EURtoUSD = 1.12;
+        const EURtoPLN = 1 / PLNtoEUR;
+        const USDtoPLN = 1 / PLNtoUSD;
+        const USDtoEUR = 1 / EURtoUSD;
 
-
-let PLNtoEUR = 0.22;
-let PLNtoUSD = 0.25;
-let EURtoUSD = 1.12;
-let EURtoPLN = 1/PLNtoEUR;
-let USDtoPLN = 1/PLNtoUSD;
-let USDtoEUR = 1/EURtoUSD;
-
-
-userValueElement.focus();
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let startValue = +userValueElement.value;
-    let startCurrency = userCurrencyElement.value;
-    let finalCurrency = chosenCurrencyElement.value;
-
-    let finalValue
-
-    switch (startCurrency+finalCurrency) {
-        case "PLNEUR":
-            finalValue = PLNtoEUR * startValue;
-            break;
-        case "PLNUSD":
-            finalValue = PLNtoUSD * startValue;
-            break;
-        case "EURUSD":
-            finalValue = EURtoUSD * startValue;
-            break;
-        case"EURPLN":
-            finalValue = EURtoPLN * startValue;
-            break;
-        case "USDEUR":
-            finalValue = USDtoEUR * startValue;
-            break;
-        case "USDPLN":
-            finalValue = USDtoPLN * startValue;
-            break;
-        default:
-            finalValue = startValue;
+        switch (startCurrency + finalCurrency) {
+            case "PLNEUR":
+                return finalValue = PLNtoEUR * startValue;
+            case "PLNUSD":
+                return finalValue = PLNtoUSD * startValue;
+            case "EURUSD":
+                return finalValue = EURtoUSD * startValue;
+            case "EURPLN":
+                return finalValue = EURtoPLN * startValue;
+            case "USDEUR":
+                return finalValue = USDtoEUR * startValue;
+            case "USDPLN":
+                return finalValue = USDtoPLN * startValue;
+            default:
+                return finalValue = startValue;
+        }
     }
 
-    finalValueElement.value = finalValue.toFixed(2);
+    const onSubmit = (event) => {
+        event.preventDefault();
 
-});
+        const userValueElement = document.querySelector(".js-userValue");
+        const userCurrencyElement = document.querySelector(".js-userCurrency");
+        const chosenCurrencyElement = document.querySelector(".js-chosenCurrency");
+        const finalValueElement = document.querySelector(".js-finalValue");
+
+        const startValue = +userValueElement.value;
+        const startCurrency = userCurrencyElement.value;
+        const finalCurrency = chosenCurrencyElement.value;
+
+        let finalValue = computeResult(startCurrency, finalCurrency, startValue);
+
+        finalValueElement.value = finalValue.toFixed(2);
+
+    }
+
+    const init = () =>{
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onSubmit);
+    }
+
+    init();
+    
+}
